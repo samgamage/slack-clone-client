09e664a0db99ba7d56d15eceab3a19cee800872b
Sam Gamage <sammygamage@gmail.com>
2018-10-28 21:17:25 -0400

    add middleware and afterware

--- src/index.js
+++ src/index.js
@@ -1,13 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
+import { ApolloClient, createNetworkInterface, ApolloProvider } from 'react-apollo';
 import 'semantic-ui-css/semantic.min.css';
+
 import Routes from './routes';
 import * as serviceWorker from './serviceWorker';
 
-const client = new ApolloClient({
+const networkInterface = createNetworkInterface({
   uri: 'http://localhost:8080/graphql',
+});
+
+networkInterface.use([
+  {
+    applyMiddleware(req, next) {
+      if (!req.options.headers) {
+        req.options.headers = {};
+      }
+
+      req.options.headers['x-token'] = localStorage.getItem('token');
+      req.options.headers['x-refresh-token'] = localStorage.getItem('refreshToken');
+      next();
+    },
+  },
+]);
+
+networkInterface.useAfter([
+  {
+    applyAfterware(
+      {
+        response: { headers },
+      },
+      next,
+    ) {
+      const token = headers.get('x-token');
+      const refreshToken = headers.get('x-refresh-token');
+
+      if (token) {
+        localStorage.setItem('token', token);
+      }
+
+      if (refreshToken) {
+        localStorage.setItem('refreshToken', refreshToken);
+      }
+
+      next();
+    },
+  },
+]);
+
+const client = new ApolloClient({
+  networkInterface,
 });
 
 const App = (
