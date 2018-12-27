e7d1f3e8af4ad404fa17d1062604be4553ed0797
sgamage2018 <sgamage2018@my.fit.edu>
2018-12-26 22:45:16 -0500

    updates

--- src/containers/MessageContainer.js
+++ src/containers/MessageContainer.js
@@ -62,20 +62,22 @@ class MessageContainer extends React.Component {
     return loading ? null : (
       <Messages>
         <Comment.Group>
-          {messages.map(m => (
-            <Comment key={`${m.id}-message`}>
-              <Comment.Content>
-                <Comment.Author as="a">{m.user.username}</Comment.Author>
-                <Comment.Metadata>
-                  <div>{m.created_at}</div>
-                </Comment.Metadata>
-                <Comment.Text>{m.text}</Comment.Text>
-                <Comment.Actions>
-                  <Comment.Action>Reply</Comment.Action>
-                </Comment.Actions>
-              </Comment.Content>
-            </Comment>
-          ))}
+          {!messages
+            ? null
+            : messages.map(m => (
+              <Comment key={`${m.id}-message`}>
+                <Comment.Content>
+                  <Comment.Author as="a">{m.user.username}</Comment.Author>
+                  <Comment.Metadata>
+                    <div>{m.created_at}</div>
+                  </Comment.Metadata>
+                  <Comment.Text>{m.text}</Comment.Text>
+                  <Comment.Actions>
+                    <Comment.Action>Reply</Comment.Action>
+                  </Comment.Actions>
+                </Comment.Content>
+              </Comment>
+            ))}
         </Comment.Group>
       </Messages>
     );
